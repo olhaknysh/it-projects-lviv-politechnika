5d6686e137fd387b44f32b5c5cfefe33b5d43356
olhaknysh <olhaknysh1994@gmail.com>
2026-05-31 20:32:23 +0300

    feat: add base structure

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// src/main.js
+const app = document.querySelector('#app');
+app.innerHTML = `
+  <div style="text-align: center; margin-top: 50px; font-family: sans-serif;">
+    <h1>Успіх! Multi-City Timeline працює на Vite ⚡</h1>
+    <p>Тепер можна будувати таймлайн подорожей.</p>
+  </div>
+`;
+
+console.log('Vite успішно запустив додаток!');
